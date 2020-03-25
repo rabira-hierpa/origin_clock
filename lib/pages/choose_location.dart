cc4361c92eba12e16ec84370fa93320c2250a666
Rabra <rabira.hierpa@gmail.com>
2020-03-25 11:14:06 +0300

    Widget Lifecycle in flutter

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -6,11 +6,36 @@ class ChoosLocation extends StatefulWidget {
 }
 
 class _ChoosLocationState extends State<ChoosLocation> {
+  int counter = 0;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    print('initState method ran');
+  }
   @override
   Widget build(BuildContext context) {
+
+    print('build method ran');
     return Scaffold(
+      backgroundColor: Colors.grey[200],
       appBar: AppBar(
         title: Text('Choose Location'),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: <Widget>[
+          Center(
+            child: RaisedButton.icon(onPressed: (){
+              setState(() {
+                counter +=1;
+              });
+            }, icon: Icon(Icons.add), label: Text('Add'))
+          ),
+          Center(
+            child: Text(counter.toString()),
+          )
+        ],
       ),
     );
   }
